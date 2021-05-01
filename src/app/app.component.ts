62f88bda9fe4b3464329bdbb179deda34a6ea039
Soma Soros <aranytestuisten@gmail.com>
2021-05-01 13:51:29 +0200

    setup project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ProjectW';
+
+  @ViewChild('glCanvas')
+  public glCanvas;
+
+  ngAfterViewInit(): void {
+    const gl: WebGL2RenderingContext = this.glCanvas.nativeElement.getContext('webgl2');
+
+  }
+
 }
